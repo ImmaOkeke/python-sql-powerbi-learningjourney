532fb8044c6af6ec14f8e0d22e9db366c6ef669d
Immaculata Okeke <immaculata.okeke@gmail.com>
2026-06-15 22:50:48 +0200

    basics - practice

---
+++ basics/practice.py
@@ -0,0 +1,32 @@
+# This is my first python program
+print("I am learning Python!")
+print("I am enjoying it")
+
+#strings
+first_name = "Immaculata"
+hobby = "coding"
+email = "immaculata.okeke@gmail.com"
+print(f"my name is {first_name}")
+print(f"my hobby is {hobby}")
+print(f"my email is:{email}")
+
+#integers
+age = 33
+quantity = 20
+print(f" I am {age} years old")
+print(f"you are buying {quantity} items")
+
+#floats
+price = 20.98
+gpa = 3.86
+distance = 10.5
+print(f"The price is ${price}")
+print(f"My gpa is: {gpa}")
+print(f"The distance is {distance }km")
+
+#booleans
+is_expensive = False
+if is_expensive:    
+    print("The wristwatch is expensive")
+else:    
+    print("The wristwatch is not expensive")
