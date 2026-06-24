2b138c9cb8ca53461ef00af7b66e3e9621815fb0
Immaculata Okeke <immaculata.okeke@gmail.com>
2026-06-24 17:17:18 +0200

    basic-practice.py

--- basics/practice.py
+++ basics/practice.py
@@ -29,4 +29,29 @@ is_expensive = False
 if is_expensive:    
     print("The wristwatch is expensive")
 else:    
-    print("The wristwatch is not expensive")
+    print("The wristwatch is Not expensive") 
+
+
+#Typecasting
+name = "Immaculata"
+age = 33
+gpa = 3.86
+is_smart = True
+
+age = float(age)
+print(age)
+
+#input
+name = input("what is your name?:")
+age = input("how old are you?:")
+age = int(age)
+age = age + 1
+print(f"hello! {name}")
+print(f"Happy Birthday!")
+print(f"you are {age} years old")
+
+#Exercise
+Length = float(input("enter the length:"))
+Width = float(input("enter the width:"))
+Area = Length * Width
+print(f"the area of the rectangle is: {Area} cm**2")
--- notes.md
+++ notes.md
@@ -1,2 +1,2 @@
 # My Learning Journey 
-Day 1: set up Python, Power BI, Git, and Github. Ready to start
+Day 1: set up Python, Power BI, Git, and Github. Ready to star
